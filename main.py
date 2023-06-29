13e05293a924be987255e8d2a6fb9559bd5f9e3f
AIandALGS <gallonzhou04@gmail.com>
2023-06-29 19:39:34 +1200

    Initial commit - window setup

---
+++ main.py
@@ -0,0 +1,36 @@
+import pygame
+
+from manager.event_manager import EventManager
+
+def main(event_manager: EventManager, screen: pygame.Surface, clock: pygame.time.Clock):
+    game_running = True
+
+    while game_running:
+        screen.fill(BLACK)
+
+        game_running = event_manager.poll_events()
+
+        pygame.display.update()
+        clock.tick(FRAME_RATE)
+
+if __name__ == "__main__":
+    import os
+
+    from src.constants import (
+        WINDOW_DISPLAY_WIDTH,
+        WINDOW_DISPLAY_HEIGHT,
+        BLACK,
+        FRAME_RATE
+    )
+
+    os.environ["SDL_VIDEO_CENTERED"] = "1"
+
+    pygame.init()
+
+    screen = pygame.display.set_mode((WINDOW_DISPLAY_WIDTH, WINDOW_DISPLAY_HEIGHT), 16)
+    
+    event_manager = EventManager()
+
+    main(event_manager, screen, pygame.time.Clock())
+
+    pygame.quit()
---
+++ manager/event_manager.py
@@ -0,0 +1,29 @@
+import pygame
+
+
+class EventManager:
+    """
+    The event manager class keeps tracks of all pygame events.
+
+    Attributes:
+    __music_manager - a private variable defined to
+    """
+
+    def __init__(self) -> None:
+        ...
+
+    def poll_events(self) -> bool:
+        """
+        Poll game events, i.e. check if the game is still running.
+
+        Return a Boolean value based on whether the game is running or
+        not.
+        """
+
+        game_running = True
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_running = False
+
+        return game_running
